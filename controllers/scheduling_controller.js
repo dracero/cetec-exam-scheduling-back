7c24dff34334686ca01db38078251a3af956930a
Manuel Collazo <collazo.manuel.28@gmail.com>
2022-08-17 20:54:38 -0300

    refactoring

--- controllers/scheduling_controller.js
+++ controllers/scheduling_controller.js
@@ -37,7 +37,7 @@ const logout = (req, res, next) => {
   req.logout(function(err) {
     if (err) { return next(err); }
     res.clearCookie("token");
-    res.redirect("http://localhost:3000/");
+    res.redirect(process.env.FRONT_URL);
     console.log(`-------> User Logged out`);
   });
 
